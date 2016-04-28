a9a51541a203e8f851e5227a43b04af4708ffc7d
guitar366 <sithinon.9@gmail.com>
2016-04-29 05:52:39 +0700

    #4
    ฝนดาวตกสำเร็จ ปุ่มควบคุมซ้ายขวา ยาน controll

--- spw/GameEngine.java
+++ spw/GameEngine.java
@@ -1,36 +1,248 @@
 package f2.spw;
- 
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.geom.Rectangle2D;
-
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.swing.Timer;
 
 
-public class GameEngine {
+public class GameEngine implements KeyListener{
 	GamePanel gp;
-	private SpaceShip v;	private Timer timer;
- 	public GameEngine(GamePanel gp, SpaceShip v) {
- 		this.gp = gp;
- 		this.v = v;
- 
- 		gp.sprites.add(v);
- 
- 		timer = new Timer(50, new ActionListener() {
- 
- 			@Override
- 			public void actionPerformed(ActionEvent arg0) {
- 				process();
- 			}
- 		});
- 		timer.setRepeats(true);
- 	}
- 
- 	public void start(){
+		
+	private ArrayList<Enemy> enemies = new ArrayList<Enemy>();	
+	private ArrayList<Bullet> bullets = new ArrayList<Bullet>();
+	private SpaceShip v;	
+	private int combo=0;
+	private Timer timer;
+	private Timer timercheck;
+	private long score = 0;
+	private double difficulty = 0.1;
+	private int hp = 100;
+	private int nc = 0;
+	private int lp = 0;
+	private long count = 0;
+	
+	public GameEngine(GamePanel gp, SpaceShip v) {
+		this.gp = gp;
+		this.v = v;		
+		
+		gp.sprites.add(v);
+		
+		timercheck = new Timer(20000, new ActionListener() {	
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+		difficulty += 0.4;
+		check();
+		count = 0;
+			}
+		});
+		
+		timer = new Timer(50, new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+		process();
+			}
+		});
+		timer.setRepeats(true);
+		timercheck.setRepeats(true);
+		
+	}
+	
+	public void start(){
 		timer.start();
- 	}
- 	private void process(){
- 		gp.updateGameUI();
- 	}
+		timercheck.start();
+	}
+	
+	private void generateEnemy(){
+		Enemy e = new Enemy((int)(Math.random()*390), 30);
+		gp.sprites.add(e);
+		enemies.add(e);
+	}
+	
+	private void process(){
+		if(Math.random() < difficulty){
+			generateEnemy();
+		}
+	
+	
+		
+		Iterator<Enemy> e_iter = enemies.iterator();
+		while(e_iter.hasNext()){
+			Enemy e = e_iter.next();
+			e.proceed();
+			
+			if(!e.isAlive()){
+				e_iter.remove();
+				gp.sprites.remove(e);
+			
+			}
+			if(e.isHit()){
+				combo++;
+				if(combo>10){
+					score += 20;
+					count += 20;
+				}
+				else{
+				score += 10;
+				count += 10;
+				}
+			}
+		}
+			
+		Iterator<Bullet> b_iter = bullets.iterator();
+		while(b_iter.hasNext()){
+			Bullet b = b_iter.next();
+			b.proceed();
+			
+			if(!b.isAlive()){
+				b_iter.remove();
+				gp.sprites.remove(b);
+			}
+		}
+		
+		gp.updateGameUI();
+		
+		Rectangle2D.Double vr = v.getRectangle();
+		Rectangle2D.Double er;
+		Rectangle2D.Double br;
+		for(Enemy e : enemies){
+			er = e.getRectangle();
+			if(er.intersects(vr)){
+				e.hitMe();
+				minus();
+				combo = 0;
+				return;
+			}
+			for(Bullet b : bullets){
+				br = b.getRectangle();
+				if(br.intersects(er)){
+					e.getHit();
+					b.getHit();
+					return;
+				}				
+			}
+		}
+	}
+	
+	public void check(){
+		if(count>=500){
+			nc += 1;
+			lp += 1;
+			}
+		if(count < 500 && count >=250){
+			lp += 1;  
+		}
+		if(count<250 && count >= 100){
+			hp = 100;
+		}
+	}
+	
+	public void minus(){
+		hp -= 10 ;
+		if(hp <= 0){
+			die();
+		}
+	
+	}
+	public void die(){
+		if(lp >= 1){
+		lifePoint();
+		lp -= 1 ;
+		}
+		else{
+		timer.stop();
+		timercheck.stop();
+		}
+	}
+	
+	public void lifePoint(){
+			hp = 100;	
+	}
+	
+	public void nuClear(){
+		if(nc >= 1){
+		nc -= 1;
+		for(Enemy e : enemies){
+			e.getHit();
+			}
+		}
+	}
+	
+	void controlVehicle(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_LEFT:
+			v.move(-1);
+			break;
+		case KeyEvent.VK_RIGHT:
+			v.move(1);
+			break;
+		/*case KeyEvent.VK_D:
+			difficulty += 0.1;
+			break;*/
+		case KeyEvent.VK_SPACE:
+			fire();
+			break;
+		case KeyEvent.VK_S:
+			nuClear();
+			break;
+		}
+	}
+	
+	public int getLp(){
+		/*int chklp = 0;
+		if(lp == true)
+			chklp = 1;*/
+		return lp;
+	}
+	
+	public int getNc(){
+		/*int chk = 0;
+		if(nc == true)
+			chk = 1;*/
+		return nc;
+	}
+	
+	public int getCombo(){
+		return combo;
+	}
+	
+	
+	public int getHp(){
+		return hp;
+	}
+
+	public long getScore(){
+		return score;
+	}
+	
+	private void fire(){
+		Bullet b = new Bullet((v.x) + (v.width/2) - 5, v.y);
+		gp.sprites.add(b);
+		bullets.add(b);
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		controlVehicle(e);
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		//do nothing
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		//do nothing		
+	}
+	
+	//public void check(int score){
+		
+	
 }
--- spw/GamePanel.java
+++ spw/GamePanel.java
@@ -9,32 +9,38 @@ import java.util.ArrayList;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
- 
- 	private BufferedImage bi;
- 	Graphics2D big;
- 	ArrayList<Sprite> sprites = new ArrayList<Sprite>();
- 
- 	public GamePanel() {
- 		bi = new BufferedImage(400, 600, BufferedImage.TYPE_INT_ARGB);
- 		big = (Graphics2D) bi.getGraphics();
- 		big.setBackground(Color.BLACK);
- 	}
- 
- 	public void updateGameUI(){
- 		big.clearRect(0, 0, 400, 600);
- 
- 		big.setColor(Color.WHITE);
- 		for(Sprite s : sprites){
- 			s.draw(big);
- 		}
- 
- 		repaint();
- 	}
- 
- 	@Override
- 	public void paint(Graphics g) {
- 		Graphics2D g2d = (Graphics2D) g;
- 		g2d.drawImage(bi, null, 0, 0);
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private BufferedImage bi;	
+	Graphics2D big;
+	ArrayList<Sprite> sprites = new ArrayList<Sprite>();
+
+	public GamePanel() {
+		bi = new BufferedImage(400, 600, BufferedImage.TYPE_INT_ARGB);
+		big = (Graphics2D) bi.getGraphics();
+		big.setBackground(Color.BLACK);
+	}
+
+	public void updateGameUI(){
+		big.clearRect(0, 0, 400, 600);
+		
+		big.setColor(Color.WHITE);
+		for(Sprite s : sprites){
+			s.draw(big);
+		}
+		repaint();
+		
+		
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		int s0 = 0;
+		g2d.drawImage(bi, null, s0, 0);
 	}
 
 }
